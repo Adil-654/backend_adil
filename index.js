2c751fc6b2e0011ef01b5fc4278c3473c634c979
Adil-654 <adilahmadshah7860@gmail.com>
2024-01-30 13:50:03 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,42 +1,128 @@
-const express=require('express')
-const app=express();
-const mongoose=require('mongoose')
+const express = require('express')
+const app = express();
+const mongoose = require('mongoose')
+const cookieparser = require('cookie-parser')
 
-const mongouri="mongodb+srv://adilahmadshah7860:adil7860@cluster0.0db5top.mongodb.net/Harsh?retryWrites=true&w=majority"
-mongoose.connect(mongouri,{ useNewUrlParser: true, useUnifiedTopology: true }).then(()=>console.log("Db connected"))
-.catch((e)=>console.log(e))
+const jwt = require('jsonwebtoken')
+app.use(cookieparser())
+const mongouri = "mongodb+srv://adilahmadshah7860:alima7860@cluster0.tlwyhtl.mongodb.net/?retryWrites=true&w=majority"
+mongoose.connect(mongouri, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => console.log("Db connected"))
+  .catch((e) => console.log(e))
 
-app.set("view engine","ejs")
-const user=[]
+const messageSchema = new mongoose.Schema({
+  name: String,
+  email: String
+})
+const user = []
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: String,
+})
+const User = mongoose.model("user", userSchema)
 
-app.use(express.urlencoded({extended:true}))
-app.get('/',(req,res)=>{
-  res.render('index')
+
+const Messge = mongoose.model("Message", messageSchema)
+app.set("view engine", "ejs")
+
+
+const isAuthenticated = (async (req, res, next) => {
+  const { token } = req.cookies;
+  if (token) {
+    const decoded = jwt.verify(token, "sdfdshasddj")
+    req.user = await User.findById(decoded._id)
+    next();
+  }
+  else {
+    res.redirect('/login')
+  }
+
 })
 
-app.get('/success',(req,res)=>{
-    
+app.use(express.urlencoded({ extended: true }))
+
+
+app.get('/', (req, res) => {
+  console.log(req.user)
+  res.render("logout", { name: req.user.name })
+})
+app.get('/add', async (req, res) => {
+  await Messge.create({ name: "adil", email: "adil@123" }).then(() => {
+    res.send("NICE")
   })
-app.get('/success',(req,res)=>{
-    res.render("sucess")
+})
+
+app.get('/login', (req, res) => {
+  res.render('login')
+})
+
+app.get('/logout', async (req, res) => {
+  res.cookie("token", null, {
+    httpOnly: true,
+    expires: new Date(Date.now())
+
   })
-
-app.post('/',(req,res)=>{
-    // console.log(req.body.name);
-    user.push({username:req.body.name,email:req.body.email})
-    res.redirect("/success")
+  res.redirect("/")
 })
 
-app.post('/contact',(req,res)=>{
-    // console.log(req.body.name);
-    user.push({username:req.body.name,email:req.body.email})
-    res.redirect("/success")
+app.get('/register', (req, res) => {
+  res.render("register")
 })
-app.get('/users',(req,res)=>{
-    res.json({
-        user
-    })
+app.post('/register', async (req, res) => {
+
+  const { name, email, password } = req.body;
+
+  let user = await User.findOne({ email })
+  if (user) {
+    res.redirect("login")
+  }
+
+  user = await User.create(({
+    name,
+    email,
+    password,
+  }))
+  const token = jwt.sign({ _id: user._id }, "sdfdshasddj")
+
+  res.cookie("token", token, {
+    httpOnly: true,
+    expires: new Date(Date.now() + 60 * 1000)
+  })
+  res.redirect("/")
 })
-app.listen(3000,()=>{
-    console.log("server started")
+app.post('/', (req, res) => {
+  // console.log(req.body.name);
+  user.push({ username: req.body.name, email: req.body.email })
+
+  res.redirect("/")
 })
+
+app.post('/login', async (req, res) => {
+  const { name, password } = req.body
+  let user = await User.findOne({ name })
+  if (!user) {
+    res.redirect('/register')
+  }
+  const isMatch = user?.password === password
+  if (!isMatch)
+    return res.render("/login", { message: "incorrrect password" })
+
+
+
+  const token = jwt.sign({ _id: user._id }, "sdfdshasddj")
+  res.cookie("token", token, {
+    httpOnly: true,
+    expires: new Date(Date.now() + 60 * 1000)
+  })
+  console.log("  ")
+  res.redirect("/index")
+
+
+})
+
+app.get('/index', (req, res) => {
+  res.redirect('/index1')
+})
+app.listen(3000, () => {
+  console.log("server started")
+})  
